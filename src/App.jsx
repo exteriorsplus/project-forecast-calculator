f908c7570f13e455161e5213510c2732fcf27214
exteriorsplus <matt@xplusconstruction.com>
2026-06-11 13:23:10 -0400

    new logic

--- src/App.jsx
+++ src/App.jsx
@@ -2008,7 +2008,7 @@ const commissionDetails = selectedTradeConfigs.map((item) => {
 const grossedUpCommission =
   grossProfit * (commissionRate / (1 - commissionRate));
 
-const commission = grossProfit * commissionRate;
+const commission = grossProfit * (commissionRate / (1 - commissionRate));
 
   return {
     ...item,
