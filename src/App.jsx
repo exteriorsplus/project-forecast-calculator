d82a5a8a6b1ea9229a77a7a8bdef291282746187
exteriorsplus <matt@xplusconstruction.com>
2026-06-16 15:38:06 -0400

    lwerj

--- src/App.jsx
+++ src/App.jsx
@@ -3236,7 +3236,7 @@ const quarterlyMotivation = (() => {
 
 const referralGoalMetMessages = [
   "the referral goal was met bub! Great work creating opportunities through relationships!",
-  "you met the referral goal bub! That means people trust you enough to send opportunities your way.",
+  "you met the referral goal! That means people trust you enough to send opportunities your way.",
   "the referral goal was achieved!  Relationship-driven business is working strong. Let's go!",
   "you reached the referral target bub. You're doing a great job!",
   "the goal was met on referrals. That kind of trust is earned not given, and that's fire."
